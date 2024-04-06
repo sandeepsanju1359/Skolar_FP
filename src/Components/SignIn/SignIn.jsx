a234109963aec5d6ac202dc3173b361c967e7e2a
sandeepsanju1358 <polakisandeep2001@gmail.com>
2024-04-06 21:25:29 +0530

    new changes

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,9 +1,8 @@
-// SignIn.js
-
 import React, { useState } from "react";
 import axios from "axios";
 import "./style.css";
 import bg from "./bg.svg";
+import Dashboard from "../Dashbard/Dashboard";
 import wave from "./wave.png";
 import avtar from "./avatar.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,11 +13,11 @@ const SignIn = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [formTitle, setFormTitle] = useState("Login"); // Initially set the form title to "Login"
+  const [formTitle, setFormTitle] = useState("Login");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleNewUserClick = () => {
     setShowNewUser(!showNewUser);
-    // Change the form title when switching between login and sign up modes
     setFormTitle(showNewUser ? "Login" : "Sign Up");
   };
 
@@ -34,13 +33,10 @@ const SignIn = () => {
         password,
       });
       alert(response.data.message);
-      // Clear input fields after successful registration
       setUsername("");
       setPassword("");
       setConfirmPassword("");
-      // Show the login form
       setShowNewUser(false);
-      // Change the form title back to "Login"
       setFormTitle("Login");
     } catch (error) {
       console.error(error);
@@ -55,12 +51,12 @@ const SignIn = () => {
         password,
       });
       alert(response.data.message);
-      // Clear input fields after successful login
       setUsername("");
       setPassword("");
+      setIsLoggedIn(true);
     } catch (error) {
       if (error.response.status === 401 || error.response.status === 404) {
-        alert("Invalid username or password"); // Display alert for invalid details
+        alert("Invalid username or password");
       } else {
         console.error(error);
       }
@@ -68,75 +64,80 @@ const SignIn = () => {
   };
 
   return (
-    <div className="card">
-      <div>
-        <img className="wave" src={wave} alt="wave" />
-        <div className="container">
-          <div className="img">
-            <img src={bg} alt="background" />
-          </div>
-          <div className="login-content">
-            <form>
-              <img src={avtar} alt="avatar" />
-              <h2 className="title">{formTitle}</h2>{" "}
-              {/* Dynamically render the form title */}
-              <div className="input-div one">
-                <div className="i">
-                  <FontAwesomeIcon icon={faUser} />
-                </div>
-                <div className="div">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
+    <div>
+      {isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        <div className="card">
+          <div>
+            <img className="wave" src={wave} alt="wave" />
+            <div className="container">
+              <div className="img">
+                <img src={bg} alt="background" />
               </div>
-              <div className="input-div pass">
-                <div className="i">
-                  <FontAwesomeIcon icon={faLock} />
-                </div>
-                <div className="div">
-                  <input
-                    type="password"
-                    className="input"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
+              <div className="login-content">
+                <form>
+                  <img src={avtar} alt="avatar" />
+                  <h2 className="title">{formTitle}</h2>{" "}
+                  <div className="input-div one">
+                    <div className="i">
+                      <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="div">
+                      <input
+                        type="text"
+                        className="input"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="input-div pass">
+                    <div className="i">
+                      <FontAwesomeIcon icon={faLock} />
+                    </div>
+                    <div className="div">
+                      <input
+                        type="password"
+                        className="input"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  {showNewUser && (
+                    <div className="input-div pass">
+                      <div className="i">
+                        <FontAwesomeIcon icon={faLock} />
+                      </div>
+                      <div className="div">
+                        <input
+                          type="password"
+                          className="input"
+                          placeholder="Confirm Password"
+                          value={confirmPassword}
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                  )}
+                  <a href="#" onClick={handleNewUserClick}>
+                    {showNewUser ? "Already User?" : "New User?"}
+                  </a>
+                  <button
+                    className="btn"
+                    onClick={showNewUser ? handleSignUp : handleSignIn}
+                  >
+                    {showNewUser ? "Sign Up" : "Login"}
+                  </button>
+                </form>
               </div>
-              {showNewUser && (
-                <div className="input-div pass">
-                  <div className="i">
-                    <FontAwesomeIcon icon={faLock} />
-                  </div>
-                  <div className="div">
-                    <input
-                      type="password"
-                      className="input"
-                      placeholder="Confirm Password"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                  </div>
-                </div>
-              )}
-              <a href="#" onClick={handleNewUserClick}>
-                {showNewUser ? "Already User?" : "New User?"}
-              </a>
-              <button
-                className="btn"
-                onClick={showNewUser ? handleSignUp : handleSignIn}
-              >
-                {showNewUser ? "Sign Up" : "Login"}
-              </button>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
